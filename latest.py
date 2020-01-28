3c5246da93c56c6704c3c4f09ed7112d978f8513
aliaaz <42954480+aliaaz@users.noreply.github.com>
2020-01-28 20:59:23 +0100

    Update latest.py

--- latest.py
+++ latest.py
@@ -2,22 +2,22 @@ import pymysql
 import scholarly
 from urllib.request import Request, urlopen
 import re
-import random
+import requests
 import hashlib
 
 # login to this url: https://remotemysql.com/phpmyadmin/sql.php?db=ZejMYc2nXj&table=papers&pos=0
 # Use the username and password as mentioned in variable 'connection'
 connection = pymysql.connect(host='37.59.55.185', user='ZejMYc2nXj', port=3306, password='TtEI93o66O', db='ZejMYc2nXj', cursorclass=pymysql.cursors.DictCursor)
-
-ai = ['"artificial intelligence"','"deep learning"','"machine learning"','"neural network"','"recurrent neural networks"',
-      '"support vector machine"','"convolutional neural network"','"decision tree"','"reinforcement learning"',
+# AI keywords
+kw = ['"artificial intelligence"','"deep learning"','"machine learning"','"neural network"','"recurrent neural networks"','"support vector machine"',
+      '"convolutional neural network"','"decision tree"','"reinforcement learning"',
       '"supervised learning"','"unsupervised learning"','"genetic algorithm"','"k fold"','"neural architecture"']
-
-security = ['Antivirus','firewall','"cyber Security"','cybersecurity','"network Security"','"internet Security"',
-            '"computer Security"','"web Security"','"intrusion detection"','"anomaly detection"','"iot security"',
-            '"mobile security"','"threat identification"','malware','spyware','ransomeware','"data leak prevention"',
-            '"data leak protection"']
-publication = ['European Symposium on Research in Computer Security','Symposium on Research in Attacks, Intrusions and Defenses']
+# Security keywords
+# kw = ['antivirus','firewall','"cyber security" OR cybersecurity','"network security"','"internet security"',
+#         '"computer security"','"web security"','"intrusion detection"','"anomaly detection"','"iot security"',
+#         '"mobile security"','"threat identification"','malware','spyware','ransomware','"data leak prevention"',
+#         '"data leak protection"']
+publication = ['European Symposium on Research in Computer Security']
 pub_type = "Security"
 keyword_type = "AI"
 pub_src = []
@@ -27,13 +27,13 @@ for n in range (len(publication)):
     pub_src.append(src + src.join(pub.split()))
 
 # data
-comb_1 = [[i,k] for i in ai
+comb_1 = [[i,k] for i in kw
                 for k in publication]
 d = []
 for m in range (len(comb_1)):
     d.append(comb_1[m])
 # search
-comb_2 = [[i,k] for i in ai
+comb_2 = [[i,k] for i in kw
                 for k in pub_src]
 search = []
 for m in range (len(comb_2)):
@@ -43,19 +43,19 @@ print("total combination:"+str(len(d)))
 count1 = 0
 search_term = "{}{}".format(search[count1][0],search[count1][1])
 
-print("combination of keyword is :" + d[count1][0],d[count1][1])#before entering the keyword into database please change the type to string
+print("combination of keyword is :" + d[count1][0],d[count1][1])
 print("the search term is:" + search_term)
 search_query = scholarly.search_pubs_query(search_term)
 data = []
 
-for x in range(100):
+for x in range(0):
     try:
         papers = next(search_query)
         data.append(papers)
         print(papers.bib['title'])
 
     except:
-        # print('skipped')
+        print('fail')
         continue
 
 # try:
@@ -64,6 +64,10 @@ with connection.cursor() as cursor:
     for y in range(len(data)):
         print(y)
 
+        try:
+            bib = data[y].url_scholarbib
+        except:
+            bib = "None"
         try:
             title = (data[y].bib['title'])
         except:
@@ -81,31 +85,36 @@ with connection.cursor() as cursor:
         except:
             url = "None"
         try:
+            eprint = (data[y].bib['eprint'])
+        except:
+            eprint = "None"
+        try:
             citedby = data[y].citedby
         except:
             citedby = 0
 
         print("citations", citedby)
 
-
+        if '…' in author:
+            author = author[0:-1]
         split_auth = author.split(' and ')
-        size_auth = len(split_auth)
-        print(title)
+        authors=[]
+        for y in range(len(split_auth)):
+            authors.append(split_auth[y].split(' '))
+        size_auth = len(authors)
 
+        print(title)
         print("url",url)
 
         hash_abs = hashlib.md5(str(str(title)+str(d[count1][1])).encode())
-        # hash_abs = hashlib.md5(str(str(title)+str(size_auth)+str(d[count1][1])).encode())
         hash_paper = hash_abs.hexdigest()
 
         # get year
         try:
-            req = Request(data[y].url_scholarbib, headers={'User-Agent': 'Mozilla/5.0'})
-            webpage = urlopen(req).read()
-            new_webpage = str(webpage)
-            split_year = new_webpage.split()
-            constraint = re.sub('[^0-9]', '', new_webpage)
-            year = constraint[:4]
+            req = Request(bib, headers={'User-Agent': 'Mozilla/5.0'})
+            sbib = str(urlopen(req).read())
+            start = sbib.find('year={')+len('year={')
+            year = sbib[start:-7]
         except:
             year = 0
 
@@ -135,8 +144,8 @@ with connection.cursor() as cursor:
             print("keyword paper id", keyword_id_1)
 
         try:
-            q_insert_paper = "INSERT IGNORE INTO papers (conference_id, title,year,search_term,citation,url,abstract,author_count,hash,date) VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s,NOW());"
-            row_insert = cursor.execute(q_insert_paper, (conf_id, title, year, search_term, citedby, url, abstract, size_auth, hash_paper))
+            q_insert_paper = "INSERT IGNORE INTO papers (conference_id, title,year,citation,url,eprint,bibtex,abstract,author_count,hash,date) VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,NOW());"
+            row_insert = cursor.execute(q_insert_paper, (conf_id, title, year, citedby, url, eprint, bib, abstract, size_auth, hash_paper))
             paper_id = connection.insert_id()
         except:
             row_insert = 0
@@ -169,20 +178,20 @@ with connection.cursor() as cursor:
             # Insert authors table and junction table
             for x in range(size_auth):
                 # Insert author
-                q_insert_author = "INSERT INTO authors (name) SELECT %s WHERE NOT EXISTS (SELECT name FROM authors WHERE name = %s)"
-                cursor.execute(q_insert_author, (split_auth[x], split_auth[x]))
+                q_insert_author = "INSERT INTO authors (firstname,lastname) SELECT %s,%s WHERE NOT EXISTS (SELECT firstname,lastname FROM authors WHERE firstname = %s AND lastname = %s)"
+                cursor.execute(q_insert_author, (authors[x][0],authors[x][1], authors[x][0],authors[x][1]))
                 author_id = connection.insert_id()
                 print("author read", author_id)
 
                 # if duplicate, get id
                 if author_id == 0:
-                    q_duplicate_author = "SELECT id FROM authors WHERE name = %s;"
-                    cursor.execute(q_duplicate_author, split_auth[x])
+                    q_duplicate_author = "SELECT id FROM authors WHERE firstname = %s AND lastname = %s;"
+                    cursor.execute(q_duplicate_author, (authors[x][0],authors[x][1]))
                     author_id = cursor.fetchone()['id']
                     print("author duplicate id", author_id)
 
                 # Insert author-paper
-                q_insert_author_paper = "INSERT INTO author_paper (paper_id, author_id) VALUES (%s,%s)"
+                q_insert_author_paper = "INSERT IGNORE INTO author_paper (paper_id, author_id) VALUES (%s,%s)"
                 cursor.execute(q_insert_author_paper, (paper_id, author_id))
                 print('new author', paper_id, author_id)
         else:
